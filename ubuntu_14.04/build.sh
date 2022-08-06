5adff4ea251ca712eb2c8ac580c323ae08492fbc
a <a@a.com>
2022-08-06 19:26:45 +0800

    update

--- ubuntu_14.04/build.sh
+++ ubuntu_14.04/build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-
-# docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build-trusty .
-
-
-userid="1001"
-groupid="1001"
-username="tom"
-
 set -x
 
-docker build --build-arg userid=$userid --build-arg groupid=$groupid --build-arg username=$username -t android-build-trusty .
+docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build-trusty .
 
--- ubuntu_18.04/build.sh
+++ ubuntu_18.04/build.sh
@@ -12,3 +12,7 @@ set -x
 
 docker build --build-arg userid=$userid --build-arg groupid=$groupid --build-arg username=$username -t android-build-bionic .
 
+
+set -x
+
+docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build-bionic .
